063103d055c2e7da57e7c3cbfaac4176b7920473
Esperanza284 <93857672+Esperanza284@users.noreply.github.com>
2023-04-12 20:41:53 +0800

    Add files via upload

---
+++ scalar_DM_Mad/couplings.f
@@ -0,0 +1,15 @@
+c This file has been generated automatically by FeynRules
+
+
+c Version: 2.3.49   Date: 8. 4. 2023,    15:0
+
+      subroutine coup(readlha)
+
+      implicit none
+      logical readlha
+
+      CALL coup1(readlha)
+      CALL coup2(readlha)
+
+      return
+      end
